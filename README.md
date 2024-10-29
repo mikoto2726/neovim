4a31b788611d952cfc071a7393ecfedb5373e3a4
mikoto2726 <mikoto27260@gmail.com>
2024-10-30 01:44:19 +0900

    fix

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 cd ~/.config  
-git clone git@github.com:mikoto2726/nvim.git
+git clone git@github.com:mikoto2726/nvim.git  
 cd nvim  
+```
 git clone --depth 1 https://github.com/wbthomason/packer.nvim \
   ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-nvim
+```
+sudo apt update
+sudo apt install -y build-essential
+nvim  
 :PackerSync
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/tk/.cache/nvim/packer_hererocks/2.1.1723681758/share/lua/5.1/?.lua;/home/tk/.cache/nvim/packer_hererocks/2.1.1723681758/share/lua/5.1/?/init.lua;/home/tk/.cache/nvim/packer_hererocks/2.1.1723681758/lib/luarocks/rocks-5.1/?.lua;/home/tk/.cache/nvim/packer_hererocks/2.1.1723681758/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/tk/.cache/nvim/packer_hererocks/2.1.1723681758/lib/lua/5.1/?.so"
+local package_path_str = "/home/kali/.cache/nvim/packer_hererocks/2.1.1723681758/share/lua/5.1/?.lua;/home/kali/.cache/nvim/packer_hererocks/2.1.1723681758/share/lua/5.1/?/init.lua;/home/kali/.cache/nvim/packer_hererocks/2.1.1723681758/lib/luarocks/rocks-5.1/?.lua;/home/kali/.cache/nvim/packer_hererocks/2.1.1723681758/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/kali/.cache/nvim/packer_hererocks/2.1.1723681758/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,88 +76,88 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ale = {
     loaded = true,
-    path = "/home/tk/.local/share/nvim/site/pack/packer/start/ale",
+    path = "/home/kali/.local/share/nvim/site/pack/packer/start/ale",
     url = "https://github.com/dense-analysis/ale"
   },
   ["auto-pairs"] = {
     loaded = true,
-    path = "/home/tk/.local/share/nvim/site/pack/packer/start/auto-pairs",
+    path = "/home/kali/.local/share/nvim/site/pack/packer/start/auto-pairs",
     url = "https://github.com/jiangmiao/auto-pairs"
   },
   ["coc.nvim"] = {
     loaded = true,
-    path = "/home/tk/.local/share/nvim/site/pack/packer/start/coc.nvim",
+    path = "/home/kali/.local/share/nvim/site/pack/packer/start/coc.nvim",
     url = "https://github.com/neoclide/coc.nvim"
   },
   ["eslint.nvim"] = {
     loaded = true,
-    path = "/home/tk/.local/share/nvim/site/pack/packer/start/eslint.nvim",
+    path = "/home/kali/.local/share/nvim/site/pack/packer/start/eslint.nvim",
     url = "https://github.com/MunifTanjim/eslint.nvim"
   },
   fzf = {
     loaded = true,
-    path = "/home/tk/.local/share/nvim/site/pack/packer/start/fzf",
+    path = "/home/kali/.local/share/nvim/site/pack/packer/start/fzf",
     url = "https://github.com/junegunn/fzf"
   },
   ["fzf.vim"] = {
     loaded = true,
-    path = "/home/tk/.local/share/nvim/site/pack/packer/start/fzf.vim",
+    path = "/home/kali/.local/share/nvim/site/pack/packer/start/fzf.vim",
     url = "https://github.com/junegunn/fzf.vim"
   },
   ["indent-blankline.nvim"] = {
     loaded = true,
-    path = "/home/tk/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
+    path = "/home/kali/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
     url = "https://github.com/lukas-reineke/indent-blankline.nvim"
   },
   ["lightline.vim"] = {
     loaded = true,
-    path = "/home/tk/.local/share/nvim/site/pack/packer/start/lightline.vim",
+    path = "/home/kali/.local/share/nvim/site/pack/packer/start/lightline.vim",
     url = "https://github.com/itchyny/lightline.vim"
   },
   nerdtree = {
     loaded = true,
-    path = "/home/tk/.local/share/nvim/site/pack/packer/start/nerdtree",
+    path = "/home/kali/.local/share/nvim/site/pack/packer/start/nerdtree",
     url = "https://github.com/preservim/nerdtree"
   },
   ["nerdtree-git-plugin"] = {
     loaded = true,
-    path = "/home/tk/.local/share/nvim/site/pack/packer/start/nerdtree-git-plugin",
+    path = "/home/kali/.local/share/nvim/site/pack/packer/start/nerdtree-git-plugin",
     url = "https://github.com/Xuyuanp/nerdtree-git-plugin"
   },
   ["nord-vim"] = {
     loaded = true,
-    path = "/home/tk/.local/share/nvim/site/pack/packer/start/nord-vim",
+    path = "/home/kali/.local/share/nvim/site/pack/packer/start/nord-vim",
     url = "https://github.com/arcticicestudio/nord-vim"
   },
   ["nvim-treesitter"] = {
-    config = { "\27LJ\2\n¬\2\0\0\5\0\14\0\0176\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0005\4\a\0=\4\b\3=\3\t\0025\3\n\0=\3\v\0025\3\f\0=\3\r\2B\0\2\1K\0\1\0\tfold\1\0\1\venable\2\vindent\1\0\1\venable\2\14highlight\fdisable\1\2\0\0\blua\1\0\2\fdisable\0\venable\2\21ensure_installed\1\0\4\vindent\0\21ensure_installed\0\14highlight\0\tfold\0\1\a\0\0\15javascript\15typescript\btsx\tjson\thtml\bcss\nsetup\28nvim-treesitter.configs\frequire\0" },
+    config = { "\27LJ\2\n¬\2\0\0\5\0\14\0\0176\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0005\4\a\0=\4\b\3=\3\t\0025\3\n\0=\3\v\0025\3\f\0=\3\r\2B\0\2\1K\0\1\0\tfold\1\0\1\venable\2\vindent\1\0\1\venable\2\14highlight\fdisable\1\2\0\0\blua\1\0\2\venable\2\fdisable\0\21ensure_installed\1\0\4\vindent\0\14highlight\0\21ensure_installed\0\tfold\0\1\a\0\0\15javascript\15typescript\btsx\tjson\thtml\bcss\nsetup\28nvim-treesitter.configs\frequire\0" },
     loaded = true,
-    path = "/home/tk/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/kali/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/tk/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/kali/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["prettier.nvim"] = {
     loaded = true,
-    path = "/home/tk/.local/share/nvim/site/pack/packer/start/prettier.nvim",
+    path = "/home/kali/.local/share/nvim/site/pack/packer/start/prettier.nvim",
     url = "https://github.com/MunifTanjim/prettier.nvim"
   },
   ["vim-gitgutter"] = {
     loaded = true,
-    path = "/home/tk/.local/share/nvim/site/pack/packer/start/vim-gitgutter",
+    path = "/home/kali/.local/share/nvim/site/pack/packer/start/vim-gitgutter",
     url = "https://github.com/airblade/vim-gitgutter"
   },
   ["vim-rainbow"] = {
     loaded = true,
-    path = "/home/tk/.local/share/nvim/site/pack/packer/start/vim-rainbow",
+    path = "/home/kali/.local/share/nvim/site/pack/packer/start/vim-rainbow",
     url = "https://github.com/frazrepo/vim-rainbow"
   },
   ["vim-snippets"] = {
     loaded = true,
-    path = "/home/tk/.local/share/nvim/site/pack/packer/start/vim-snippets",
+    path = "/home/kali/.local/share/nvim/site/pack/packer/start/vim-snippets",
     url = "https://github.com/honza/vim-snippets"
   }
 }
@@ -165,7 +165,7 @@ _G.packer_plugins = {
 time([[Defining packer_plugins]], false)
 -- Config for: nvim-treesitter
 time([[Config for nvim-treesitter]], true)
-try_loadstring("\27LJ\2\n¬\2\0\0\5\0\14\0\0176\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0005\4\a\0=\4\b\3=\3\t\0025\3\n\0=\3\v\0025\3\f\0=\3\r\2B\0\2\1K\0\1\0\tfold\1\0\1\venable\2\vindent\1\0\1\venable\2\14highlight\fdisable\1\2\0\0\blua\1\0\2\fdisable\0\venable\2\21ensure_installed\1\0\4\vindent\0\21ensure_installed\0\14highlight\0\tfold\0\1\a\0\0\15javascript\15typescript\btsx\tjson\thtml\bcss\nsetup\28nvim-treesitter.configs\frequire\0", "config", "nvim-treesitter")
+try_loadstring("\27LJ\2\n¬\2\0\0\5\0\14\0\0176\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0005\4\a\0=\4\b\3=\3\t\0025\3\n\0=\3\v\0025\3\f\0=\3\r\2B\0\2\1K\0\1\0\tfold\1\0\1\venable\2\vindent\1\0\1\venable\2\14highlight\fdisable\1\2\0\0\blua\1\0\2\venable\2\fdisable\0\21ensure_installed\1\0\4\vindent\0\14highlight\0\21ensure_installed\0\tfold\0\1\a\0\0\15javascript\15typescript\btsx\tjson\thtml\bcss\nsetup\28nvim-treesitter.configs\frequire\0", "config", "nvim-treesitter")
 time([[Config for nvim-treesitter]], false)
 
 _G._packer.inside_compile = false
